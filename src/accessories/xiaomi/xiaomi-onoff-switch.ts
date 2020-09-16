dd08b1402fa3a9921366ccf60e6a31fd702a6310
Korneluk Wadim <58448955+xrust83@users.noreply.github.com>
2020-09-17 00:34:55 +0300

    Update xiaomi-onoff-switch.ts

--- src/accessories/xiaomi/xiaomi-onoff-switch.ts
+++ src/accessories/xiaomi/xiaomi-onoff-switch.ts
@@ -1,75 +1,61 @@
 import { ZigBeeAccessory } from '../zig-bee-accessory';
-import {
-  CharacteristicEventTypes,
-  CharacteristicGetCallback,
-  CharacteristicSetCallback,
-  Service,
-} from 'homebridge';
+import { Service } from 'homebridge';
 import { DeviceState } from '../../zigbee/types';
 import { Device } from 'zigbee-herdsman/dist/controller/model';
+import { ProgrammableSwitchServiceBuilder } from '../../builders/programmable-switch-service-builder';
 
-export class XiaomiOnoffSwitch extends ZigBeeAccessory {
-  protected switchServiceButton: Service;
+export class IkeaOnoffSwitch extends ZigBeeAccessory {
+  protected switchServiceOn: Service;
+  protected switchServiceOff: Service;
 
   getAvailableServices(): Service[] {
-    const Characteristic = this.platform.api.hap.Characteristic;
-
-    this.switchServiceButton =
-      this.accessory.getServiceById(
-        this.platform.Service.StatefulProgrammableSwitch,
-        'button'
-      ) ||
-      this.accessory.addService(
-        this.platform.Service.StatefulProgrammableSwitch,
-        'Button',
-        'button'
-      );
-
-    this.switchServiceButton.setCharacteristic(
-      this.platform.Characteristic.Name,
-      'Button'
+    const builder = new ProgrammableSwitchServiceBuilder(
+      this.platform,
+      this.accessory,
+      this.client,
+      this.state
     );
-    this.switchServiceButton.setCharacteristic(
-      this.platform.Characteristic.ServiceLabelIndex,
-      1
-    );
-    this.switchServiceButton
-      .getCharacteristic(Characteristic.ProgrammableSwitchOutputState)
-      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-        callback(null, this.state.state === 'ON' ? 1 : 0);
-      })
-      .on(
-        CharacteristicEventTypes.SET,
-        async (outputState: number, callback: CharacteristicSetCallback) => {
-          try {
-            await this.client.setLeftButtonOn(this.zigBeeDeviceDescriptor, outputState === 1);
-            callback(null, outputState);
-          } catch (e) {
-            callback(e);
-          }
-        }
-      );
 
-   return [this.switchServiceButton];
+    const ProgrammableSwitchEvent = this.platform.Characteristic.ProgrammableSwitchEvent;
+    [this.switchServiceOn, this.switchServiceOff] = builder
+      .withStatelessSwitch('ON', 'on', 1, [
+        ProgrammableSwitchEvent.SINGLE_PRESS,
+        ProgrammableSwitchEvent.LONG_PRESS,
+      ])
+      .withStatelessSwitch('OFF', 'off', 2, [
+        ProgrammableSwitchEvent.SINGLE_PRESS,
+        ProgrammableSwitchEvent.LONG_PRESS,
+      ])
+      .andBattery()
+      .build();
+
+    return [this.switchServiceOn, this.switchServiceOff];
   }
 
   update(device: Device, state: DeviceState) {
+    const ProgrammableSwitchEvent = this.platform.Characteristic.ProgrammableSwitchEvent;
     super.update(device, state);
-    const Characteristic = this.platform.api.hap.Characteristic;
-    switch (state.state_left) {
-      case 'ON':
-        this.switchServiceButton.setCharacteristic(
-          Characteristic.ProgrammableSwitchOutputState,
-          1
-        );
+    switch (state.click) {
+      case 'brightness_up':
+        this.switchServiceOn
+          .getCharacteristic(ProgrammableSwitchEvent)
+          .setValue(ProgrammableSwitchEvent.LONG_PRESS);
         break;
-      case 'OFF':
-        this.switchServiceButton.setCharacteristic(
-          Characteristic.ProgrammableSwitchOutputState,
-          0
-        );
+      case 'brightness_down':
+        this.switchServiceOff
+          .getCharacteristic(ProgrammableSwitchEvent)
+          .setValue(ProgrammableSwitchEvent.LONG_PRESS);
+        break;
+      case 'on':
+        this.switchServiceOn
+          .getCharacteristic(ProgrammableSwitchEvent)
+          .setValue(ProgrammableSwitchEvent.SINGLE_PRESS);
+        break;
+      case 'off':
+        this.switchServiceOff
+          .getCharacteristic(ProgrammableSwitchEvent)
+          .setValue(ProgrammableSwitchEvent.SINGLE_PRESS);
         break;
     }
-   
   }
 }
