3435c2366e9120c1685af0bb2a8e189a66f77143
Piotr Kaczmarek <75645851+piokacz@users.noreply.github.com>
2021-01-29 11:34:25 +0100

    Create tuya-temp-humi-sensor.ts

---
+++ src/accessories/tuya/tuya-temp-humi-sensor.ts
@@ -0,0 +1,42 @@
+import { ZigBeeAccessory } from '../zig-bee-accessory';
+import { Service } from 'homebridge';
+import { TemperatureSensorServiceBuilder } from '../../builders/temperature-sensor-service-builder';
+import { HumiditySensorServiceBuilder } from '../../builders/humidity-sensor-service-builder';
+import { BatteryServiceBuilder } from '../../builders/battery-service-builder';
+
+export class TuyaTempHumiSensor extends ZigBeeAccessory {
+  private temperatureService: Service;
+  private humidityService: Service;
+  private batteryService: Service;
+
+  getAvailableServices() {
+    this.temperatureService = new TemperatureSensorServiceBuilder(
+      this.platform,
+      this.accessory,
+      this.client,
+      this.state
+    )
+      .withTemperature()
+      .build();
+
+    this.humidityService = new HumiditySensorServiceBuilder(
+      this.platform,
+      this.accessory,
+      this.client,
+      this.state
+    )
+      .withHumidity()
+      .build();
+
+    this.batteryService = new BatteryServiceBuilder(
+      this.platform,
+      this.accessory,
+      this.client,
+      this.state
+    )
+      .withBatteryPercentage()
+      .build();
+
+    return [this.temperatureService, this.humidityService, this.batteryService];
+  }
+}
