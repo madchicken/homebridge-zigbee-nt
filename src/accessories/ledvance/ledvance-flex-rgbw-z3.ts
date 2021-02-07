16c6e1200d8c1f28a931973a555ddd2cf049b814
KoNi17 <74485531+KoNi17@users.noreply.github.com>
2021-02-07 15:45:35 +0100

    added 2 ledvance devices

---
+++ src/accessories/ledvance/ledvance-flex-rgbw-z3.ts
@@ -0,0 +1,37 @@
+import { LighbulbServiceBuilder } from '../../builders/lighbulb-service-builder';
+import { IkeaTradfriDim } from '../ikea/ikea-tradfri-dim';
+import { ZigbeeNTHomebridgePlatform } from '../../platform';
+import { PlatformAccessory } from 'homebridge';
+import { ZigBeeClient } from '../../zigbee/zig-bee-client';
+import { Device } from 'zigbee-herdsman/dist/controller/model';
+
+export class LedvanceFlexRgbwZ3 extends IkeaTradfriDim {
+  constructor(
+    platform: ZigbeeNTHomebridgePlatform,
+    accessory: PlatformAccessory,
+    client: ZigBeeClient,
+    device: Device
+  ) {
+    super(platform, accessory, client, device);
+  }
+
+  getAvailableServices() {
+    this.lightbulbService = new LighbulbServiceBuilder(
+      this.platform,
+      this.accessory,
+      this.client,
+      this.state
+    )
+      .withOnOff()
+      .withBrightness()
+      .withColorXY()
+      .build();
+    return [this.lightbulbService];
+  }
+
+  async onDeviceMount() {
+    await super.onDeviceMount();
+    const color = await this.client.getColorCapabilities(this.zigBeeDeviceDescriptor);
+    this.log.info(`Re-read color capabilities for ${this.name}`, color);
+  }
+}
